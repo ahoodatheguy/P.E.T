c26e19836be531ea0cfb9ecd2c934cb17ff3eee8
Ahadu Kebede <ahadukebede@gmail.com>
2023-08-05 17:22:06 -0400

    Added missing `requests` module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name="P.E.T",
 	version="1.0",
-	install_requires=["typer", "rich", 'pyexiftool'],
+	install_requires=["requests", "typer", "rich", 'pyexiftool'],
 	entry_points={
 		"console_scripts": [
 			"pet = pet.__main__:main"
